6380ded99d15359bb1baeccd3f21b69f2c25d2a0
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2020-08-30 15:03:18 +0200

    Starter customization

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Layout = props => (
+    <>
+        <div></div>
+        <main>
+            {props.children}
+        </main>
+    </>
+);
+
+export default Layout;
