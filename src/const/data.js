38e198b5204f5add89fa80dc2aecde2a8913403d
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2020-08-30 21:46:34 +0200

    Add constant data

---
+++ src/const/data.js
@@ -0,0 +1,14 @@
+export const INGREDIENTS = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+
+export const INGREDIENTS_PRICES = {
+  salad: 1,
+  bacon: 1,
+  cheese: 1,
+  meat: 1
+};
