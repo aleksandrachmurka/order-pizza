d9f260d5ffd6653b5d07aa7df9b2320fe43a8fa5
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2020-09-06 18:02:25 +0200

    Add responsive navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styles from './App.module.css'
 import Toolbar from './components/Navigation/Toolbar/Toolbar';
 import Order from './containers/Order';
+import SideDrawerToggler from './components/SideDrawer/SideDrawerToggler';
+import SideDrawer from './components/SideDrawer/SideDrawer';
 
+class App extends Component {
+	state = {
+		showSideDrawer: false,
+	}
 
-const App = () => (
-	<>
-		<Toolbar />
-		<Order />
-	</>
-);
+	toggleSideDrawerHandler = () => {
+		this.setState(prevState => ({ showSideDrawer: !prevState.showSideDrawer }))
+	}
+
+	render() {
+		return (
+			<>
+				<header>
+					<SideDrawerToggler show={this.state.showSideDrawer} toggleShow={this.toggleSideDrawerHandler} />
+					<SideDrawer show={this.state.showSideDrawer} toggleShow={this.toggleSideDrawerHandler} />
+					<Toolbar />
+				</header>
+				<main className={this.state.showSideDrawer ? styles.expanded : styles.main}>
+					<Order />
+				</main>
+			</>
+		)
+	}
+};
 
 export default App;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import toolbar from './Toolbar.module.css';
+import Logo from '../Logo/Logo';
+import Navigation from '../Navigation/Navigation';
 
-const Toolbar = () => (
-  <header className={toolbar.toolbar}>
-    <div>MENU</div>
-    <div>LOGO</div>
-    <nav>
-
-    </nav>
-  </header>
+const Toolbar = ({ showSideDrawer, toggleShowSideDrawer }) => (
+  <div className={toolbar.toolbar}>
+    <Logo />
+    <Navigation />
+  </div>
 );
 
 export default Toolbar;
